aee05512e7cb459238e61c494512353d755e0b81
ErneDainesi <edainesi@fi.uba.ar>
2024-03-25 19:22:02 -0300

    remove if

--- yt/yt.go
+++ yt/yt.go
@@ -8,9 +8,5 @@ import (
 func Download(message string) error {
     url := strings.Split(message, " ")
     runCmd := exec.Command("yt-dlp", url[1])
-    err := runCmd.Run()
-    if err != nil {
-        return err
-    }
-    return nil
+    return runCmd.Run()
 }
